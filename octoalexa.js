f7f4b7a7eb527e664b9ce1cd2c3a76a7576c76a6
ZaviiNet <trancefx@live.co.uk>
2017-12-06 21:57:55 +0000

    Update octoalexa.js

--- octoalexa.js
+++ octoalexa.js
@@ -2,7 +2,7 @@ exports.handler = function( event, context ) {
 
     var http = require( 'http' );
 
-    var url = 'Your OctoPrint Link & API Key Goes here!';
+    var url = 'http://Your_IP/api/job?apikey=YourAPIKEY';
 
     http.get( url, function( response ) {
 
@@ -13,9 +13,13 @@ exports.handler = function( event, context ) {
         response.on( 'end', function() {
 
             var json = JSON.parse( data );
-
+            var hours = Math.floor(json.progress.printTimeLeft / 3600);
+            var minutes = Math.floor(json.progress.printTimeLeft / 60) % 60
+            var seconds = Math.floor(json.progress.printTimeLeft % 60);
             var text = 'Your 3D Print has ';
-            text+=json.progress.printTimeLeft+" Minutes Remaining";
+            text+=hours+" Hours ";
+            text+=minutes+" Minutes and ";
+            text+=seconds+" Seconds Remaining ";
             output( text, context );
 
         } );
